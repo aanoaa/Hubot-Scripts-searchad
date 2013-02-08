1a7f70569bac5417ef4844d53a987f4ffa26295d
Hyungsuk Hong <aanoaa@gmail.com>
2013-02-08 17:10:15 +0900

    impl `hubot searchad bundleid:rank`

--- lib/Hubot/Scripts/searchad.pm
+++ lib/Hubot/Scripts/searchad.pm
@@ -4,26 +4,51 @@ package Hubot::Scripts::searchad;
 use strict;
 use warnings;
 
+use WWW::Naver::SearchAd;
+
 sub load {
     my ( $class, $robot ) = @_;
 
-    ## robot respond only called its name first. `hubot xxx`
-    $robot->respond(
-        qr/pattern (.*)?$/i, # (.*) will captured as `$msg->match->[0]`
-        sub {
-            my $msg  = shift;    # Hubot::Response
-            $msg->send('hi');    # hubot> hi
-            $msg->reply('hi');   # hubot> user: hi
-        }
-    );
+    my $ad = WWW::Naver::SearchAd->new;
 
-    ## robot can hear anything
-    $robot->hear(
-        qr/^pattern (.*)?$/i,
+    $robot->respond(
+        qr/searchad (.*)?$/i,
         sub {
-            my $msg  = shift;    # Hubot::Response
+            my $msg  = shift;
+            my @items = item_filter(split / /, $msg->match->[0]);
+
+            return $msg->send('no items for bidding') unless @items;
+
+            my $autheticated = $ad->signin(
+                $ENV{HUBOT_SEARCHAD_NAVER_USERNAME},
+                $ENV{HUBOT_SEARCHAD_NAVER_PASSWORD}
+            );
+
+            return $msg->send($ad->{error}) unless $autheticated;
+
+            for my $item (@items) {
+                my ($bundle_id, $rank) = split /:/, $item;
+                if ($ad->refresh($bundle_id, $rank)) {
+                    $msg->send("[$bundle_id] ranked to [$rank] successfully");
+                } else {
+                    $msg->send($ad->{error});
+                }
+            }
         }
     );
+}
+
+sub item_filter {
+    my @items = @_;
+
+    my @validated_items;
+    for my $item (@items) {
+        my ($bundle_id, $rank) = split /:/, $item;
+        next if (!$bundle_id || !$rank);
+        push @validated_items, $item;
+    }
+
+    return @validated_items;
 }
 
 1;
@@ -37,7 +62,7 @@ Hubot::Scripts::searchad
 
 =head1 SYNOPSIS
 
-    hubot <command> <args> - help message here
+    hubot searchad <bundleid>:<rank> ... - start to bidding for each <bundleid> to <rank>
 
 =head1 DESCRIPTION
 
@@ -47,17 +72,19 @@ need more description?
 
 =over
 
-=item HUBOT_ENV_VARS_TO_SET
+=item HUBOT_SEARCHAD_NAVER_USERNAME
+
+=item HUBOT_SEARCHAD_NAVER_PASSWORD
 
 =back
 
 =head1 AUTHOR
 
-Your name <your@email.com>
+Hyungsuk Hong <aanoaa@gmail.com>
 
 =head1 COPYRIGHT AND LICENSE
 
-This software is copyright (c) 2012 by <Your Name>.
+This software is copyright (c) 2012 by Hyungsuk Hong.
 
 This is free software; you can redistribute it and/or modify it under
 the same terms as the Perl 5 programming language system itself.
